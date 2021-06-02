82c5d77e648605637588f869ff9050f8dd4c0c5d
kOF.Readie <kof.readie@outlook.com>
2021-06-02 08:22:50 +0100

    Allow for parts of a file to be requested.

--- src/files/storage/index.php
+++ src/files/storage/index.php
@@ -31,11 +31,45 @@ class File
         }
 
         //https://stackoverflow.com/questions/1628260/downloading-a-file-with-a-different-name-to-the-stored-name
-        header('Content-Disposition: attachment; filename="' . $files->data[0]->name. '.' . $files->data[0]->type . '"');
-        header("Content-Length: " . filesize(__DIR__ . '/userfiles/' . $files->data[0]->id));
-        header("Content-Type: " . mime_content_type(__DIR__ . '/userfiles/' . $files->data[0]->id));
-        $fileContents = readfile(__DIR__ . '/userfiles/' . $files->data[0]->id);
-        if ($fileContents === false) { http_response_code(500); exit(); }
+        //https://stackoverflow.com/questions/5924061/using-php-to-output-an-mp4-video
+
+
+        $fileSize = filesize(__DIR__ . '/userfiles/' . $files->data[0]->id);
+        //This shouldn't fail to open because I checked if the file existed earlier.
+        $fileStream = fopen(__DIR__ . '/userfiles/' . $files->data[0]->id, 'rb');
+
+        $beginBytes = 0;
+        $endBytes = $fileSize;
+
+        if (isset($_SERVER['HTTP_RANGE']) && preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i', $_SERVER['HTTP_RANGE'], $matches))
+        {
+            $beginBytes=intval($matches[0]);
+            if(!empty($matches[1]))
+            {
+                $endBytes=intval($matches[1]);
+            }
+        }
+
+        if($beginBytes > 0|| $endBytes < $fileSize) { http_response_code(206); }
+        else { http_response_code(200); }
+
+        header('Content-Type: ' . mime_content_type(__DIR__ . '/userfiles/' . $files->data[0]->id));
+        header('Accept-Ranges: bytes');
+        header('Content-Length:' . ($endBytes - $beginBytes));
+        header("Content-Disposition: inline;");
+        header("Content-Range: bytes $beginBytes-$endBytes/$fileSize");
+        header("Content-Transfer-Encoding: binary\n");
+        header('Connection: close');
+
+        $cursor = $beginBytes;
+        fseek($fileStream, $beginBytes, 0);
+
+        while(!feof($fileStream) && $cursor < $endBytes && (connection_status()==0))
+        {
+            echo fread($fileStream, min(1024 * 16, $endBytes - $cursor));
+            $cursor += 1024 * 16;
+            usleep(1000);
+        }
         exit();
     }
 }
