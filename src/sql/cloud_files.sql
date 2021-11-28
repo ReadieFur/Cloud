599fa779eaab3f8c224b7df5a34997d987e247d7
Tristan Read <60425965+kOFReadie@users.noreply.github.com>
2021-11-28 03:03:13 +0000

    Update cloud_files.sql

--- src/sql/cloud_files.sql
+++ src/sql/cloud_files.sql
@@ -4,6 +4,7 @@ CREATE TABLE `cloud_files` (
     `name` varchar(256) NOT NULL DEFAULT 'Untitled file',
     `type` char(128) DEFAULT NULL,
     `size` varchar(128) NOT NULL DEFAULT '0',
+    `metadata` varchar(256) NOT NULL,
     `shareType` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0 - private,\r\n1 - invite,\r\n2 - public',
     `dateAltered` varchar(32) NOT NULL,
     FOREIGN KEY (`uid`) REFERENCES `cloud_permissions`(`uid`)
